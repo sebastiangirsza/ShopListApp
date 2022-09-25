bc5a18c1c0a87ca76e564db2fdcc736dd1ffc0c4
Sebastian Girsza <sebag96@wp.pl>
2022-09-25 12:30:30 +0200

    Add Splash Screen

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -22,8 +22,97 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.transparent,
         canvasColor: const Color.fromARGB(255, 2, 116, 209),
       ),
-      home: const RootPage(),
+      home: const SplashScreen(),
     );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SplashScreen();
+  }
+}
+
+class _SplashScreen extends State<SplashScreen> {
+  int splashtime = 1;
+
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: splashtime), () async {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+        return const RootPage();
+      }));
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                stops: [0.1, 0.5, 0.7, 0.9],
+                colors: [
+                  Color.fromARGB(255, 40, 40, 40),
+                  Color.fromARGB(255, 60, 60, 60),
+                  Color.fromARGB(255, 80, 80, 80),
+                  Color.fromARGB(255, 100, 100, 100),
+                ],
+              ),
+            ),
+            alignment: Alignment.center,
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  const AnimatedSize(
+                    curve: Curves.easeIn,
+                    duration: Duration(seconds: 3),
+                    child: CircleAvatar(
+                      radius: 110,
+                      backgroundColor: Color.fromARGB(255, 0, 63, 114),
+                      child: CircleAvatar(
+                        backgroundImage: AssetImage('images/icon/app_icon.png'),
+                        radius: 100,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 30),
+                    child: const Text(
+                      'Shop List App',
+                      style: TextStyle(
+                        fontSize: 30,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 15),
+                    child: ListView(
+                      shrinkWrap: true,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Text('by Sebastian Girsza',
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                )),
+                            SizedBox(height: 15),
+                            Text('version 1.0.0'),
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                ])));
   }
 }
 
