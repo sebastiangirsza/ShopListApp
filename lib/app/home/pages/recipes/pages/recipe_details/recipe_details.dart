1e54faa274362c7b1a7668799efb1314a990f2ca
Sebastian Girsza <sebag96@wp.pl>
2022-11-06 11:59:34 +0100

    Extract method in RecipeDetails

--- lib/app/home/pages/recipes/pages/recipe_details/recipe_details.dart
+++ lib/app/home/pages/recipes/pages/recipe_details/recipe_details.dart
@@ -51,171 +51,131 @@ class _RecipeDatailsState extends State<RecipeDatails> {
           padding: const EdgeInsets.symmetric(horizontal: 15),
           child: ListView(
             children: [
-              Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10)),
-                  color: Color.fromARGB(255, 0, 63, 114),
-                  boxShadow: <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 2,
-                      offset: Offset(3, 3),
-                    )
-                  ],
-                ),
-                margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                child: Center(
-                  child: Text('Potrzebne składniki',
-                      style: GoogleFonts.getFont('Saira',
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold)),
-                ),
-              ),
-              Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10)),
-                  color: Colors.white,
-                  boxShadow: <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 2,
-                      offset: Offset(3, 3),
-                    )
-                  ],
-                ),
-                margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
-                child: Text(widget.recipesModel.recipesProductName,
-                    style: GoogleFonts.getFont('Saira',
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold)),
-              ),
-              Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      topLeft: Radius.circular(10),
-                      topRight: Radius.circular(10)),
-                  color: Color.fromARGB(255, 0, 63, 114),
-                  boxShadow: <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 2,
-                      offset: Offset(3, 3),
-                    )
-                  ],
-                ),
-                margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
-                child: Center(
-                  child: Text('Sposób przygotowania',
-                      style: GoogleFonts.getFont('Saira',
-                          color: Colors.white,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold)),
-                ),
-              ),
-              Container(
-                decoration: const BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(10),
-                      bottomRight: Radius.circular(10)),
-                  color: Colors.white,
-                  boxShadow: <BoxShadow>[
-                    BoxShadow(
-                      color: Colors.black,
-                      blurRadius: 2,
-                      offset: Offset(3, 3),
-                    )
-                  ],
-                ),
-                margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                child: Text(widget.recipesModel.recipesMakeing,
-                    textAlign: TextAlign.justify,
-                    style: GoogleFonts.getFont('Saira',
-                        color: Colors.black,
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold)),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    const Icon(
-                      Icons.arrow_circle_down_outlined,
-                      color: Color.fromARGB(255, 0, 63, 114),
-                    ),
-                    Flexible(
-                      flex: 1,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Flexible(
-                            child: Text(
-                              'Sprawdź czy posiadasz potrzebne składniki:',
-                              style: GoogleFonts.getFont('Saira',
-                                  fontSize: 12,
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.end,
-                            ),
-                          ),
-                          const SizedBox(width: 10),
-                          SizedBox(
-                            width: 90,
-                            child: TextField(
-                              maxLength: 1,
-                              cursorColor: Colors.grey,
-                              decoration: InputDecoration(
-                                  counterText: '',
-                                  fillColor: Colors.black.withOpacity(0.3),
-                                  filled: true,
-                                  border: OutlineInputBorder(
-                                      borderRadius: BorderRadius.circular(10),
-                                      borderSide: BorderSide.none),
-                                  hintText: 'Wyszukaj potrzebne produkty',
-                                  hintStyle: GoogleFonts.getFont('Saira',
-                                      color: Colors.grey, fontSize: 12),
-                                  prefixIcon: Container(
-                                    padding: const EdgeInsets.all(15),
-                                    width: 18,
-                                    child: Icon(
-                                      Icons.search,
-                                      color:
-                                          const Color.fromARGB(255, 0, 63, 114)
-                                              .withOpacity(0.5),
-                                    ),
-                                  )),
-                              onChanged: (value) {
-                                setState(() {
-                                  searchKey = value;
-                                });
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+              recipesDetailTitle('Potrzebne składniki'),
+              recipesDetailContent(widget.recipesModel.recipesProductName, 18),
+              recipesDetailTitle('Sposób przygotowania'),
+              recipesDetailContent(widget.recipesModel.recipesMakeing, 15),
+              searchingWidget(),
               if (searchKey != '') _SearchResults(searchKey: searchKey),
+              const SizedBox(height: 15)
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget recipesDetailTitle(String title) {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(10), topRight: Radius.circular(10)),
+        color: Color.fromARGB(255, 0, 63, 114),
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black,
+            blurRadius: 2,
+            offset: Offset(3, 3),
+          )
+        ],
+      ),
+      margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+      child: Center(
+        child: Text(title,
+            style: GoogleFonts.getFont('Saira',
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold)),
+      ),
+    );
+  }
+
+  Widget recipesDetailContent(String content, double fontSize) {
+    return Container(
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(10), bottomRight: Radius.circular(10)),
+        color: Colors.white,
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            color: Colors.black,
+            blurRadius: 2,
+            offset: Offset(3, 3),
+          )
+        ],
+      ),
+      margin: const EdgeInsets.only(left: 10, right: 10, bottom: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 5),
+      child: Text(content,
+          style: GoogleFonts.getFont('Saira',
+              color: Colors.black,
+              fontSize: fontSize,
+              fontWeight: FontWeight.bold)),
+    );
+  }
+
+  Widget searchingWidget() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 5),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const Icon(
+            Icons.arrow_circle_down_outlined,
+            color: Color.fromARGB(255, 0, 63, 114),
+          ),
+          Flexible(
+            flex: 1,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Flexible(
+                  child: Text(
+                    'Sprawdź czy posiadasz potrzebne składniki:',
+                    style: GoogleFonts.getFont('Saira',
+                        fontSize: 12,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.end,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                SizedBox(
+                  width: 90,
+                  child: TextField(
+                    maxLength: 1,
+                    cursorColor: Colors.grey,
+                    decoration: InputDecoration(
+                        counterText: '',
+                        fillColor: Colors.black.withOpacity(0.3),
+                        filled: true,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide.none),
+                        hintText: 'Wyszukaj potrzebne produkty',
+                        hintStyle: GoogleFonts.getFont('Saira',
+                            color: Colors.grey, fontSize: 12),
+                        prefixIcon: Container(
+                          padding: const EdgeInsets.all(15),
+                          width: 18,
+                          child: Icon(
+                            Icons.search,
+                            color: const Color.fromARGB(255, 0, 63, 114)
+                                .withOpacity(0.5),
+                          ),
+                        )),
+                    onChanged: (value) {
+                      setState(() {
+                        searchKey = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
@@ -242,42 +202,45 @@ class _SearchResults extends StatelessWidget {
             shrinkWrap: true,
             children: [
               for (final purchasedProduct in purchasedProducts) ...[
-                Column(
-                  children: [
-                    const SizedBox(height: 2),
-                    Container(
-                      padding: const EdgeInsets.symmetric(
-                          vertical: 5, horizontal: 15),
-                      decoration: const BoxDecoration(
-                        color: Color.fromARGB(255, 255, 255, 255),
-                        boxShadow: <BoxShadow>[
-                          BoxShadow(
-                            color: Colors.black,
-                            blurRadius: 2,
-                            offset: Offset(3, 3),
-                          )
-                        ],
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 2),
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 15),
+                        decoration: const BoxDecoration(
+                          color: Color.fromARGB(255, 255, 255, 255),
+                          boxShadow: <BoxShadow>[
+                            BoxShadow(
+                              color: Colors.black,
+                              blurRadius: 2,
+                              offset: Offset(3, 3),
+                            )
+                          ],
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              purchasedProduct.purchasedProductName,
+                              style: GoogleFonts.getFont('Saira',
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              purchasedProduct.storageName,
+                              style: GoogleFonts.getFont('Saira',
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            purchasedProduct.purchasedProductName,
-                            style: GoogleFonts.getFont('Saira',
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          Text(
-                            purchasedProduct.storageName,
-                            style: GoogleFonts.getFont('Saira',
-                                color: Colors.black,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 )
               ],
             ],
