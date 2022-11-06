9f45a81ed85d39ef045c9ea4f70cd5dc5aac8316
Sebastian Girsza <sebag96@wp.pl>
2022-11-06 21:51:43 +0100

    Change login page color

--- lib/app/login/login_page.dart
+++ lib/app/login/login_page.dart
@@ -19,70 +19,86 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(isCreatingAccount == true
-                  ? 'Zarejestruj się'
-                  : 'Zaloguj się'),
-              TextField(
-                controller: widget.emailController,
-              ),
-              TextField(
-                controller: widget.passwordController,
-                obscureText: true,
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  if (isCreatingAccount == true) {
-                    try {
-                      context.read<AuthCubit>().createUser(
-                            email: widget.emailController.text,
-                            password: widget.passwordController.text,
-                          );
-                    } catch (error) {
-                      const Text('ERROR');
-                    }
-                  } else {
-                    try {
-                      context.read<AuthCubit>().logIn(
-                            email: widget.emailController.text,
-                            password: widget.passwordController.text,
-                          );
-                    } catch (error) {
-                      const Text('ERROR');
-                    }
-                  }
-                },
-                child: Text(
-                    isCreatingAccount == true ? 'Utwórz konto' : 'Zaloguj się'),
-              ),
-              if (isCreatingAccount == false) ...[
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      isCreatingAccount = true;
-                    });
-                  },
-                  child: const Text('Utwórz konto'),
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          stops: [0.1, 0.5, 0.7, 0.9],
+          colors: [
+            Color.fromARGB(255, 200, 233, 255),
+            Color.fromARGB(255, 213, 238, 255),
+            Color.fromARGB(255, 228, 244, 255),
+            Color.fromARGB(255, 244, 250, 255),
+          ],
+        ),
+      ),
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(isCreatingAccount == true
+                    ? 'Zarejestruj się'
+                    : 'Zaloguj się'),
+                TextField(
+                  controller: widget.emailController,
                 ),
-              ],
-              if (isCreatingAccount == true) ...[
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      isCreatingAccount = false;
-                    });
-                  },
-                  child: const Text('Masz już konto?'),
+                TextField(
+                  controller: widget.passwordController,
+                  obscureText: true,
                 ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () async {
+                    if (isCreatingAccount == true) {
+                      try {
+                        context.read<AuthCubit>().createUser(
+                              email: widget.emailController.text,
+                              password: widget.passwordController.text,
+                            );
+                      } catch (error) {
+                        const Text('ERROR');
+                      }
+                    } else {
+                      try {
+                        context.read<AuthCubit>().logIn(
+                              email: widget.emailController.text,
+                              password: widget.passwordController.text,
+                            );
+                      } catch (error) {
+                        const Text('ERROR');
+                      }
+                    }
+                  },
+                  child: Text(isCreatingAccount == true
+                      ? 'Utwórz konto'
+                      : 'Zaloguj się'),
+                ),
+                if (isCreatingAccount == false) ...[
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isCreatingAccount = true;
+                      });
+                    },
+                    child: const Text('Utwórz konto'),
+                  ),
+                ],
+                if (isCreatingAccount == true) ...[
+                  TextButton(
+                    onPressed: () {
+                      setState(() {
+                        isCreatingAccount = false;
+                      });
+                    },
+                    child: const Text('Masz już konto?'),
+                  ),
+                ],
               ],
-            ],
+            ),
           ),
         ),
       ),
